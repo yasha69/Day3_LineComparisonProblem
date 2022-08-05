561e5777a942a50abd664cf8ef49dfb2d9971097
Yash <yashatkulwar2000@gmail.com>
2022-08-05 21:23:25 +0530

    added welcome message

---
+++ LineComparison.java
@@ -0,0 +1,7 @@
+package LineComparisonProblem;
+
+public class LineComparison {
+    public static void main(String args []) {
+        System.out.println("WELCOME TO LINE COMPARISON COMPUTATION PROGRAM ");
+    }
+}
